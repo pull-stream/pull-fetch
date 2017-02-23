dc460ecce763e8e47dd680ebf45d383ea076ed2e
Matt Ross <amsross@gmail.com>
2017-02-23 06:41:51 -0500

    Remove redundant tests

--- test/test.js
+++ test/test.js
@@ -21,34 +21,10 @@ test('fetch contents regularly', function (t) {
         pull.values([ { foo: 'bar' } ]),
         pull.map(JSON.stringify),
         fetch('https://example.com/', { method: 'POST', body: 'application/json' }),
-        pull.asyncMap((resp, done) => {
-          return pull(resp, pull.collect(done))
-        }),
-        pull.drain((data) => {
-          t.equal(body, '{"foo":"bar"}', 'POST body was correctly sent');
-          t.deepEqual(data, [new Buffer('ok')], 'result is buffer containing response');
-        }, t.end));
-    });
-
-    t.test('fetch should should trigger completion callback', function(t) {
-
-      var body;
-      nock('https://example.com')
-        .post('/')
-        .reply(function(uri, requestBody) {
-          body = requestBody;
-          return [201, 'ok'];
-        });
-
-      pull(
-        pull.values([ { foo: 'bar' } ]),
-        pull.map(JSON.stringify),
-        fetch('https://example.com/', { method: 'POST', body: 'application/json' }),
-        pull.asyncMap((resp, done) => {
-          return pull(resp, pull.collect(done))
-        }),
+        pull.asyncMap((resp, done) => pull(resp, pull.collect(done))),
         pull.map(Buffer.concat),
         pull.drain((data) => {
+          t.equal(body, '{"foo":"bar"}', 'POST body was correctly sent');
           t.deepEqual(data, new Buffer('ok'), 'result is buffer containing response');
         }, t.end));
     });
@@ -60,19 +36,6 @@ test('fetch contents regularly', function (t) {
   t.test('result', function(t) {
 
     t.test('result should be returned', function(t) {
-
-      nock('https://example.com')
-        .get('/')
-        .reply(200, '{"msg": "success"}')
-
-      pull(
-        fetch.result('https://example.com'),
-        pull.drain((data) => {
-          t.deepEqual(data, new Buffer('{"msg": "success"}'), 'result is buffer containing response');
-        }, t.end));
-    });
-
-    t.test('result should trigger completion callback', function(t) {
 
       nock('https://example.com')
         .get('/')
@@ -101,19 +64,6 @@ test('fetch contents regularly', function (t) {
         fetch.json('https://example.com'),
         pull.drain((data) => {
           t.deepEqual(data, {'msg': 'success'}, 'returned json is parsed');
-        }, t.end));
-    });
-
-    t.test('json should trigger completion callback', function(t) {
-
-      nock('https://example.com')
-        .get('/')
-        .reply(200, '{"msg": "success"}')
-
-      pull(
-        fetch.json('https://example.com'),
-        pull.drain((data) => {
-          t.deepEqual(data, {'msg': 'success'}, 'response was received');
         }, t.end));
     });
 
